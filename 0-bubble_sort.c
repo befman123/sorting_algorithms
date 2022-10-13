ddb73c488ab3d51ebe9b2dc76b01859ff2353d2d
befman123 <befman123@gmail.com>
2022-10-13 13:08:39 +0300

    Corrected many errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,19 +10,15 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
-	for (i = 0; i < size - 1; i++)
-	{
+	for (i = 0; i < size - i; i++)
 		for (j = 0; j < size - 1; j++)
 		{
-			if (*(array + j) > *(array + j + 1))
+			if (array[j] > array[j + 1])
 			{
-				temp = *(array + j);
-				*(array + j) = *(array + j + 1);
-				*(array + j + 1) = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
-			else
-				continue;
-			print_array(array, size);
 		}
-	}
 }
